f4b20e983afc2082707f69153ff1d1ae5a08b0c9
Fernando <villafae>
2020-12-26 10:19:14 -0300

    arreglos de formulario

--- main.js
+++ main.js
@@ -2,19 +2,20 @@ window.addEventListener('DOMContentLoaded', function () {
   // get the form elements defined in your form HTML above
 
   var form = document.getElementById('my-form');
-  var button = document.getElementById('my-form-button');
-  var status = document.getElementById('my-form-status');
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById('status');
 
   // Success and Error functions for after the form is submitted
 
   function success() {
     form.reset();
-    button.style = 'display: none ';
-    status.innerHTML = 'gracias!';
+    status.classList.add('success');
+    status.innerHTML = 'Enviado, Gracias!';
   }
 
   function error() {
-    status.innerHTML = 'Oops! Houston we have a problem.';
+    status.classList.add('error');
+    status.innerHTML = 'Tenemos un problema, revisá los campos!';
   }
 
   // handle the form submission event
